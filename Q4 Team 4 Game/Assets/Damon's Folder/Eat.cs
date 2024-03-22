e163d7adbdf061acfbdc6f05be385c8d98ddf9a4
Bradbury <1717930@mukilteo.wednet.edu>
2024-03-22 08:06:37 -0700

    fruit thingy

---
+++ Q4 Team 4 Game/Assets/Damon's Folder/Eat.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour
+{
+    
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
---
+++ Q4 Team 4 Game/Assets/Damon's Folder/Stats.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public int health;
+    public int attack;
+    public bool hasEatenJFruit;
+    public bool hasEatenGFruit;
+    public bool hasEatenFFruit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
