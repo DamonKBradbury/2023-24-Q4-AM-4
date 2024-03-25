34ced7911a3857320ae6d130a0f4ea9dd21fac07
Bradbury <1717930@mukilteo.wednet.edu>
2024-03-25 09:05:22 -0700

    fruits

--- Q4 Team 4 Game/Assets/Damon's Folder/Eat.cs
+++ Q4 Team 4 Game/Assets/Damon's Folder/Eat.cs
@@ -4,9 +4,57 @@ using UnityEngine;
 
 public class Eat : MonoBehaviour
 {
+    public string fruitName;
     
-    public void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter2D (Collision2D collision)
     {
-        
-    }
+        if ((collision.gameObject.GetComponent("Stats") as Stats) != null)
+        {
+
+             if (fruitName == "Jump Fruit")
+             {
+
+                if (collision.gameObject.GetComponent<Stats>().hasEatenFFruit == true)
+                {
+                    collision.gameObject.GetComponent<Stats>().hasEatenFFruit = false;
+                } 
+                else if (collision.gameObject.GetComponent<Stats>().hasEatenGFruit == true)
+                {
+                    collision.gameObject.GetComponent<Stats>().hasEatenGFruit = false;
+                }
+
+                collision.gameObject.GetComponent<Stats>().hasEatenJFruit = true;
+             }
+             else if (fruitName == "Fire Fruit")
+             {
+
+                if (collision.gameObject.GetComponent<Stats>().hasEatenJFruit == true)
+                {
+                    collision.gameObject.GetComponent<Stats>().hasEatenJFruit = false;
+                }
+                else if (collision.gameObject.GetComponent<Stats>().hasEatenGFruit == true)
+                {
+                    collision.gameObject.GetComponent<Stats>().hasEatenGFruit = false;
+                }
+
+                collision.gameObject.GetComponent<Stats>().hasEatenFFruit = true;
+             }
+              else if (fruitName == "Ghost Fruit")
+             {
+                
+                if (collision.gameObject.GetComponent<Stats>().hasEatenJFruit == true)
+                {
+                    collision.gameObject.GetComponent<Stats>().hasEatenJFruit = false;
+                }
+                else if (collision.gameObject.GetComponent<Stats>().hasEatenFFruit == true)
+                {
+                    collision.gameObject.GetComponent<Stats>().hasEatenFFruit = false;
+                }
+
+                collision.gameObject.GetComponent<Stats>().hasEatenGFruit = true;
+             }
+
+          Destroy(gameObject);
+        }
+    } 
 }
--- Q4 Team 4 Game/Assets/Damon's Folder/Stats.cs
+++ Q4 Team 4 Game/Assets/Damon's Folder/Stats.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
+    public int maxHealth;
     public int health;
-    public int attack;
+    public int baseDamage;
+    public int damage;
+
     public bool hasEatenJFruit;
     public bool hasEatenGFruit;
     public bool hasEatenFFruit;
+    public int fruitTimer;
+
+
+    // Damage player
+    public void OnCollisionEnter(Collision collision)
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+        if (collision.gameObject.GetComponent<Stats>().damage >= 1)
+        {
+            health -= collision.gameObject.GetComponent<Stats>().damage;
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            // Death
+            if (health <= 0)
+            {
+                
+            }
+        }
+    }
+
 
     // Start is called before the first frame update
     void Start()
